d8aa9988c18bedc865e787f26a2d2973ad3e7ee6
cnov20 <128@holbertonschool.com>
2017-07-11 14:57:56 +0000

    Switched order of commands

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
 -- Remove all records of table <= certain value, from database on MYSQL server
-DELETE `score` FROM second_table WHERE `score` <= 5 ORDER BY score desc;
+DELETE FROM second_table WHERE score <= 5 ORDER BY score desc;
