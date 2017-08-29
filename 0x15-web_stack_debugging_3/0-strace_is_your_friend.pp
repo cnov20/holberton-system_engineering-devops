54457f9d7fd94e25862cf93f827c13e8073b417b
cnov20 <128@holbertonschool.com>
2017-08-29 05:41:38 +0000

    Added exec command to install stdlibmodule if not previously installed

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,17 @@
 # This manifest automates web server / web stack debugging
 # By finding and replacing erroneously named Wordpress filename in WP settings
 
+exec {
+  command => 'puppet module install puppetlabs-stdlib'
+}
+
 file { '/var/www/html/wp-settings.php':
   include stdlib
   ensure => present,
 }->
+
 file_line { 'Append a line to /var/www/html/wp-settings.php':
-  path => '/var/www/html/wp-settings.php',
-  line => '.php',
-  match   => ".phpp"
+  path  => '/var/www/html/wp-settings.php',
+  line  => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' )",
+  match => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' )"
+}
