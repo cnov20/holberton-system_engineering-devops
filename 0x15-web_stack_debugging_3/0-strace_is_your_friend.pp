145cce2aaa5e8883afffb061ff2f963219fda578
cnov20 <128@holbertonschool.com>
2017-08-29 06:29:33 +0000

    Fixed for puppet lint specification

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 # By finding and replacing erroneously named Wordpress filename in WP settings
 
 exec { 'Modify a line of /var/www/html/wp-settings':
-  path  => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
 }
