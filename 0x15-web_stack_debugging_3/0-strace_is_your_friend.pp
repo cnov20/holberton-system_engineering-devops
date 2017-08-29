dfd4ec9510b486b9166c7693f127b387d086a72c
cnov20 <128@holbertonschool.com>
2017-08-29 06:27:40 +0000

    Re-factored / Simplified approach using 'sed' rather than puppet modules and file_line resource

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,7 @@
 # This manifest automates web server / web stack debugging
 # By finding and replacing erroneously named Wordpress filename in WP settings
 
-exec {
-  command => 'puppet module install puppetlabs-stdlib'
-}
-
-file { '/var/www/html/wp-settings.php':
-  include stdlib
-  ensure => present,
-}
-
-file_line { 'Append a line to /var/www/html/wp-settings.php':
-  path  => '/var/www/html/wp-settings.php',
-  line  => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' )",
-  match => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' )"
+exec { 'Modify a line of /var/www/html/wp-settings':
+  path  => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
 }
