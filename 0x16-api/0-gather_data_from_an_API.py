0ce74800873c11f9cba090f19e4ca2ab0769eadd
cnov20 <128@holbertonschool.com>
2017-09-13 13:54:50 +0000

    Changed import order

--- 0x16-api/0-gather_data_from_an_API.py
+++ 0x16-api/0-gather_data_from_an_API.py
@@ -6,9 +6,10 @@ for a given employee ID, returns information
 about his/her TODO list progress.
 '''
 
-from sys import argv
 import requests
 import json
+from sys import argv
+
 
 if __name__ == '__main__':
 
