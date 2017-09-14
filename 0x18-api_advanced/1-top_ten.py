d8517bc0c7e51c42ace514c9becb72ee415a3ba5
cnov20 <128@holbertonschool.com>
2017-09-14 15:23:17 +0000

    Re-factored and simplified - Realized score calculation of posts was not necessary and needed to slice list of dictionaries(posts) BEFORE looping through

--- 0x18-api_advanced/1-top_ten.py
+++ 0x18-api_advanced/1-top_ten.py
@@ -15,23 +15,28 @@ def top_ten(subreddit):
     subreddit: given subreddit to be make request to
     '''
 
-    headers = {'User-Agent': 'Mozilla/5.0; (Macintosh Intel Mac OS X 10_10_1)'
-               'AppleWebKit/537.36 (KHTML, like Gecko)'
-               'Chrome/39.0.2171.95 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0; (Macintosh Intel Mac OS X 10_10_1)'
+        'AppleWebKit/537.36 (KHTML, like Gecko)'
+        'Chrome/39.0.2171.95 Safari/537.36'
+    }
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
     response = requests.get(url, headers=headers).json()
     if response.get('error'):
         print (None)
     else:
-        postsList = response.get('data').get('children')
-        scoreList = []
+        topTen = []
+        postsList = response.get('data').get('children')[0:10]
         for post in postsList:
             data = post.get('data')
-            score = data.get('score')
-            scoreList.append(score)
-            scoreList.sort()
-            topTen = scoreList[-10:]
+            title = data.get('title')
+            print(title)
 
-            if score in topTen:
+'''
+            scoreList.append(score)
+
+            for i in range(len(topTen)):
                 title = data.get('title')
-                print(title)
+                if score in topTen:
+                    print(title)
+'''
