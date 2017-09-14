66b99e04ba5d11eb9da112e1a7058b8b6b7fcd02
cnov20 <128@holbertonschool.com>
2017-09-14 02:20:54 +0000

    Complete re-factor based on previous web_stack_debugging_3 project - Use sed to replace U_LIMIT in /etc/default/nginx file

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
-# This manifest creates a puppet file, to execute a command
-# Specifically, that modifies the U_LIMIT on an nginx web server'
-exec { 'sed; sudo service nginx restart':
-  path    => ['/etc/default/nginx'],
-  command => 'sed s/15/4096/g',
-  returns => '0'
+# This manifest automates web server / web stack debugging
+# By finding and replacing erroneously named Wordpress filename in WP settings
+
+exec { 'Modify a line of /etc/default/nginx':
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i 's/15/4096/g' /etc/default/nginx; sudo service nginx restart"
 }
