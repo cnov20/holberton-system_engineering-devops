1083a07780cee7d54f30aac43001f7713bf7ca67
cnov20 <128@holbertonschool.com>
2017-09-14 13:28:56 +0000

    Re-implemented (un-commented) error handling if subreddit (parameter) is invalid

--- 0x18-api_advanced/1-top_ten.py
+++ 0x18-api_advanced/1-top_ten.py
@@ -20,18 +20,18 @@ def top_ten(subreddit):
                'Chrome/39.0.2171.95 Safari/537.36'}
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
     response = requests.get(url, headers=headers).json()
-    '''    if response.get('error'):
-    print (None)
-    else: '''
-    postsList = response.get('data').get('children')
-    scoreList = []
-    for post in postsList:
-        data = post.get('data')
-        score = data.get('score')
-        scoreList.append(score)
-        scoreList.sort()
-        topTen = scoreList[-10:]
+    if response.get('error'):
+        print (None)
+    else:
+        postsList = response.get('data').get('children')
+        scoreList = []
+        for post in postsList:
+            data = post.get('data')
+            score = data.get('score')
+            scoreList.append(score)
+            scoreList.sort()
+            topTen = scoreList[-10:]
 
-        if score in topTen:
-            title = data.get('title')
-            print(title)
+            if score in topTen:
+                title = data.get('title')
+                print(title)
