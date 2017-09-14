2783fa3eb09fa994778dd923f200c278452944ab
cnov20 <128@holbertonschool.com>
2017-09-14 13:23:48 +0000

    Added Task 1 - query Reddit API - find and return the top ten 'hot' posts for a given subreddit, passed in as parameter to function

---
+++ 0x18-api_advanced/1-top_ten.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+'''
+This is module utilizes the Reddit API to make API calls,
+which query for the top ten 'hot' posts for a given subreddit
+'''
+
+import requests
+
+
+def top_ten(subreddit):
+
+    '''
+    Method finds the top ten 'hot' posts in given subreddit
+    subreddit: given subreddit to be make request to
+    '''
+
+    headers = {'User-Agent': 'Mozilla/5.0; (Macintosh Intel Mac OS X 10_10_1)'
+               'AppleWebKit/537.36 (KHTML, like Gecko)'
+               'Chrome/39.0.2171.95 Safari/537.36'}
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    response = requests.get(url, headers=headers).json()
+    '''    if response.get('error'):
+    print (None)
+    else: '''
+    postsList = response.get('data').get('children')
+    scoreList = []
+    for post in postsList:
+        data = post.get('data')
+        score = data.get('score')
+        scoreList.append(score)
+        scoreList.sort()
+        topTen = scoreList[-10:]
+
+        if score in topTen:
+            title = data.get('title')
+            print(title)
